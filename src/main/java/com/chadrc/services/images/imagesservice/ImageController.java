e1aa5b894a11052a024ced9ea8fa553f69bb3bff
chadcollins <chadrcollins91@gmail.com>
2017-09-20 21:45:08 -0400

    Added support for configurable root to upload to

--- src/main/java/com/chadrc/services/images/imagesservice/ImageController.java
+++ src/main/java/com/chadrc/services/images/imagesservice/ImageController.java
@@ -2,12 +2,14 @@ package com.chadrc.services.images.imagesservice;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.PostConstruct;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -15,9 +17,19 @@ import java.io.IOException;
 public class ImageController {
     private final Logger log = LoggerFactory.getLogger(ImageController.class);
 
+    @Value("${service.images.store.root}")
+    private String storeRoot;
+
+    @PostConstruct
+    public void postConstruct() {
+        if (!storeRoot.endsWith("/")) {
+            storeRoot += "/";
+        }
+    }
+
     @PostMapping
     public ResponseEntity uploadImage(@RequestParam("image") MultipartFile image) throws IOException {
-        FileOutputStream outputStream = new FileOutputStream(image.getOriginalFilename());
+        FileOutputStream outputStream = new FileOutputStream(storeRoot + image.getOriginalFilename());
         outputStream.write(image.getBytes());
         return ResponseEntity.ok(null);
     }
