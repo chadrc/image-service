c45387fc900745f96dcf543e278bda3c82343967
chadcollins <chadrcollins91@gmail.com>
2017-09-22 21:21:12 -0400

    Setup opencv and first part of build process

---
+++ buildRun.sh
@@ -0,0 +1,11 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+./gradlew clean
+
+./gradlew build
+
+unzip build/distributions/images-service-0.0.1.zip -d build/distributions/
+
+export JAVA_OPTS="-Djava.library.path=$DIR/lib -Dspring.profiles.active=local"
+
+sh build/distributions/images-service-0.0.1/bin/images-service
--- src/main/java/com/chadrc/services/images/imagesservice/ImagesServiceApplication.java
+++ src/main/java/com/chadrc/services/images/imagesservice/ImagesServiceApplication.java
@@ -1,12 +1,28 @@
 package com.chadrc.services.images.imagesservice;
 
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ImagesServiceApplication {
+	private static final Logger log = LoggerFactory.getLogger(ImagesServiceApplication.class);
 
 	public static void main(String[] args) {
+		openCVCheck();
 		SpringApplication.run(ImagesServiceApplication.class, args);
+	}
+
+	private static void openCVCheck() {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+		System.out.println("mat = " + mat.dump());
+
+		log.info("OpenCV initialized");
+		log.info("OpenCV Version: " + Core.VERSION);
 	}
 }
