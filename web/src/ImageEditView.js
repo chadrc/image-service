27bf8bfc546bfed5cbdba075c7cacebde15d7f4a
chadcollins <chadrcollins91@gmail.com>
2017-09-25 21:43:52 -0400

    Moved html into class

--- web/src/ImageEditView.js
+++ web/src/ImageEditView.js
@@ -1,55 +1,6 @@
 import React, {Component} from 'react';
 import Globals from './Globals';
 
-const View = ({image, onImageLoad, setImage, onSubmit, onBackButtonClicked, currentWidth, currentHeight}) => (
-    <div>
-        <button type="button" className="btn btn-info mb-2" onClick={() => onBackButtonClicked()}>
-            Back
-        </button>
-        <div className="card">
-            <div className="card-header">
-                <h3 className="card-title">Edit Image</h3>
-            </div>
-            <div className="card-block">
-                <div className="row">
-                    <div className="col-sm-3">
-                        <form onSubmit={(event) => onSubmit(event)}>
-                            <section className="form-group">
-                                <label htmlFor="imageName">Name</label>
-                                <input id="imageName" name="name" className="form-control-file" />
-                            </section>
-                            <div className="form-group">
-                                <label>Size</label>
-                                <div>
-                                    <p className="form-control-static">{image.size}</p>
-                                </div>
-                            </div>
-                            <button type="button" className="btn btn-primary">Save</button>
-                        </form>
-                    </div>
-                    <div className="col-sm-9">
-                        <div className="pos-rel">
-                            <img onLoad={() => onImageLoad()}
-                                 ref={(img) => setImage(img)}
-                                 className="w-100"
-                                 src={`${Globals.ImageUrl}/${image.name}`} />
-                            {image.focalPoints.map((point, index) => {
-                                return (
-                                    <div key={image.name + "-fp-" + index}
-                                         className="pos-abs dot" style={{
-                                        top: point.y * currentHeight,
-                                        left: point.x * currentWidth
-                                    }}/>
-                                )
-                            })}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-);
-
 class ImageEditView extends Component {
     constructor(props) {
         super(props);
@@ -78,13 +29,58 @@ class ImageEditView extends Component {
     render() {
         let width = this.image ? this.image.width : 0;
         let height = this.image ? this.image.height : 0;
-        return <View image={this.props.image}
-                     currentWidth={width}
-                     currentHeight={height}
-                     setImage={(img) => this.image = img}
-                     onImageLoad={() => this.onImageLoad()}
-                     onSubmit={(event) => this.props.onSubmit(event)}
-                     onBackButtonClicked={() => this.props.onBackButtonClicked()}/>;
+        let image=this.props.image;
+        let currentWidth=width;
+        let currentHeight=height;
+
+        return (
+            <div>
+                <button type="button" className="btn btn-info mb-2" onClick={() => this.props.onBackButtonClicked()}>
+                    Back
+                </button>
+                <div className="card">
+                    <div className="card-header">
+                        <h3 className="card-title">Edit Image</h3>
+                    </div>
+                    <div className="card-block">
+                        <div className="row">
+                            <div className="col-sm-3">
+                                <form onSubmit={(event) => this.props.onSubmit(event)}>
+                                    <section className="form-group">
+                                        <label htmlFor="imageName">Name</label>
+                                        <input id="imageName" name="name" className="form-control-file" />
+                                    </section>
+                                    <div className="form-group">
+                                        <label>Size</label>
+                                        <div>
+                                            <p className="form-control-static">{image.size}</p>
+                                        </div>
+                                    </div>
+                                    <button type="button" className="btn btn-primary">Save</button>
+                                </form>
+                            </div>
+                            <div className="col-sm-9">
+                                <div className="pos-rel">
+                                    <img onLoad={() => this.onImageLoad()}
+                                         ref={(img) => this.image = img}
+                                         className="w-100"
+                                         src={`${Globals.ImageUrl}/${image.name}`} />
+                                    {image.focalPoints.map((point, index) => {
+                                        return (
+                                            <div key={image.name + "-fp-" + index}
+                                                 className="pos-abs dot" style={{
+                                                top: point.y * currentHeight,
+                                                left: point.x * currentWidth
+                                            }}/>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 
     componentDidUpdate() {
