463a7f6d543a23fcdf92042da3756e59979cb483
chadcollins <chadrcollins91@gmail.com>
2017-10-01 15:36:11 -0400

    Added form reducer, little clean up

--- web/src/Store.js
+++ web/src/Store.js
@@ -3,17 +3,15 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import createHistory from 'history/createBrowserHistory'
 import { routerReducer, routerMiddleware } from 'react-router-redux'
 import { composeWithDevTools } from 'redux-devtools-extension';
-
+import { reducer as formReducer } from 'redux-form';
 import reducers from './reducers'
-// Create a history of your choosing (we're using a browser history in this case)
+
 const history = createHistory();
-
-// Build the middleware for intercepting and dispatching navigation actions
 const middleware = routerMiddleware(history);
-
 const store = createStore(
     combineReducers({
         ...reducers,
+        form: formReducer,
         router: routerReducer
     }),
     composeWithDevTools(applyMiddleware(middleware))
